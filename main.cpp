a80228d3f0ae32c9dfa2d29078cb183a1e176be0
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-06 17:04:54 +0100

    Update main.cpp
    testing AudioBuffer

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "miosix.h"
 #include "audio.h"
 #include "audio_processor.h"
+#include "audio_buffer.h"
 #include <functional>
 #include <math.h>
 
@@ -10,17 +11,19 @@ using namespace std;
 
 // testing an implementation of an AudioProcessor
 #define BUFFSIZE 256
+
 class AudioProcessorTest : public AudioProcessor {
 public:
     AudioProcessorTest() {
-        for(int i=0; i < BUFFSIZE; ++i) {
+        for (int i = 0; i < BUFFSIZE; ++i) {
             sinTable[i] = 4095 * ((sin(2 * 3.14 * i / BUFFSIZE) + 1) / 2);
         }
 
     }
+
     inline void process() override {
-        auto* b = getBuffer();
-        for (int i=0; i < getBufferSize(); ++i) {
+        auto *b = getBuffer();
+        for (int i = 0; i < getBufferSize(); ++i) {
             b[i] = sinTable[i];
         }
     }
@@ -37,6 +40,13 @@ int main() {
 
     // setting the audio processor
     audioDriver.setAudioProcessable(audioProcessorTest);
+
+
+    // audio buffer test
+    AudioBuffer<unsigned int, 2, 256> buffer;
+    auto rawRead = buffer.getReadPointer(0);
+    auto rawWrite = buffer.getWritePointer(1);
+    rawWrite[256 - 1] = rawRead[0];
 
     // infinite loop
     for (;;) {
