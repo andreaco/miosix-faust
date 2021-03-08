451307bdf45a8bd885e37cd805455295ce32b90f
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-08 19:34:28 +0100

    audio_buffer
    move operator and copy constructor disabled.
    Assertion on BUFFER_LEN

--- audio_buffer.cpp
+++ audio_buffer.cpp
@@ -1,6 +1,7 @@
 
-
+#include "miosix.h"
 #include "audio_buffer.h"
+#include <cstdint>
 
-//template<typename T, size_t CHANNEL_NUM, size_t BUFFER_LEN>
-//AudioBuffer::AudioBuffer() {};
+
+
--- audio_buffer.h
+++ audio_buffer.h
@@ -7,23 +7,47 @@
 
 #include <array>
 
+// TODO: limit the BUFFER_LEN to be even
 template<typename T, size_t CHANNEL_NUM, size_t BUFFER_LEN>
 class AudioBuffer {
 public:
-    AudioBuffer() {};
+    AudioBuffer() {
+        static_assert((BUFFER_LEN % 2) == 0, "The AudioBuffer BUFFER_LEN must be even");
+    };
 
     inline size_t getNumChannels() { return bufferContainer.size(); };
 
     inline size_t getBufferLength() { return bufferContainer[0].size(); };
 
     inline const T *getReadPointer(unsigned int channelNumber) { return bufferContainer[channelNumber].data(); };
-    inline T *getWritePointer(unsigned int channelNumber) { return bufferContainer[channelNumber].data(); };
 
-    void addBuffer(AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN>) {}; // TODO: SIMD implementation
-    void applyGain(float) {}; // TODO: SIMD implementation
+    inline T *getWritePointer(unsigned int channelNumber) { return bufferContainer[channelNumber].data(); };
+    
+    void applyGain(float) {};
 
 private:
     std::array <std::array<T, BUFFER_LEN>, CHANNEL_NUM> bufferContainer;
+    AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN>(const AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN>&);
+    AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN> &operator=(const AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN>&);
 };
+
+// TODO: SIMD implementation
+//namespace AudioBufferSIMD {
+//    template<size_t CHANNEL_NUM, size_t BUFFER_LEN>
+//    void add_uint16_t(AudioBuffer<uint16_t, CHANNEL_NUM, BUFFER_LEN> buffer1,
+//                      AudioBuffer<uint16_t, CHANNEL_NUM, BUFFER_LEN> buffer2) {
+//        for (int i = 0; i < CHANNEL_NUM; i++) { // iterating for each channel
+//            // these pointers contain two uint16_t values
+//            uint32_t *p1 = static_cast<uint32_t *>(buffer1.getWritePointer(i));
+//            uint32_t *p2 = static_cast<uint32_t *>(buffer2.getWritePointer(i));
+//
+//            // uint32_t  [0:BUFFER_LEN/2-1]: one value = two uint16_t values
+//            // uint16_t [0:BUFFER_LEN-1]
+//            for (unsigned int j = 0; j < BUFFER_LEN / 2; j++) {
+//                p1[j] = __QADD16(p1[j], p2[j]);
+//            }
+//        }
+//    };
+//}
 
 #endif //MIOSIX_AUDIO_AUDIO_BUFFER_H
