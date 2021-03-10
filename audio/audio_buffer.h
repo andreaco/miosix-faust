241f75b195715c6fae3c6d2997900d3a3368ec64
andreaco <andrea.cppl@gmail.com>
2021-03-10 17:51:03 +0100

    Removed SIMD instructions

--- audio/audio_buffer.h
+++ audio/audio_buffer.h
@@ -88,23 +88,4 @@ private:
     AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN> &operator=(const AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN> &);
 };
 
-// TODO: SIMD implementation
-//namespace AudioBufferSIMD {
-//    template<size_t CHANNEL_NUM, size_t BUFFER_LEN>
-//    void add_uint16_t(AudioBuffer<uint16_t, CHANNEL_NUM, BUFFER_LEN> buffer1,
-//                      AudioBuffer<uint16_t, CHANNEL_NUM, BUFFER_LEN> buffer2) {
-//        for (int i = 0; i < CHANNEL_NUM; i++) { // iterating for each channel
-//            // these pointers contain two uint16_t values
-//            uint32_t *p1 = static_cast<uint32_t *>(buffer1.getWritePointer(i));
-//            uint32_t *p2 = static_cast<uint32_t *>(buffer2.getWritePointer(i));
-//
-//            // uint32_t  [0:BUFFER_LEN/2-1]: one value = two uint16_t values
-//            // uint16_t [0:BUFFER_LEN-1]
-//            for (unsigned int j = 0; j < BUFFER_LEN / 2; j++) {
-//                p1[j] = __QADD16(p1[j], p2[j]);
-//            }
-//        }
-//    };
-//}
-
 #endif //MIOSIX_AUDIO_AUDIO_BUFFER_H
