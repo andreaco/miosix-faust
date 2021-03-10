000cc3a0a1e0eb3373cd258c79dcec414813f17e
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-10 11:38:28 +0100

    Update audio_buffer.cpp/h
    add() and applyGain() methods added. Comments

--- audio/audio_buffer.cpp
+++ audio/audio_buffer.cpp
@@ -1,7 +1,29 @@
 
 #include "miosix.h"
 #include "audio_buffer.h"
-#include <cstdint>
 
 
+template<typename T, size_t CHANNEL_NUM, size_t BUFFER_LEN>
+void AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN>::applyGain(float gain) {
+    for (uint32_t channelNumber = 0; channelNumber < getNumChannels(); channelNumber++) {
+        // iterating for each channel
+        T* channel = getWritePointer(channelNumber);
+        for (uint32_t sampleIndex = 0; sampleIndex < getBufferLength(); sampleIndex++) {
+            // applying the gain to each sample of the channel
+            channel[sampleIndex] *= gain;
+        }
+    }
+}
 
+template<typename T, size_t CHANNEL_NUM, size_t BUFFER_LEN>
+void AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN>::add(AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN> buffer) {
+    for (uint32_t channelNumber = 0; channelNumber < getNumChannels(); channelNumber++) {
+        // iterating for each channelBuffer1
+        T* channelBuffer1 = getWritePointer(channelNumber);
+        T* channelBuffer2 = buffer.getWritePointer(channelNumber);
+        for (uint32_t sampleIndex = 0; sampleIndex < getBufferLength(); sampleIndex++) {
+            // summing AudioBuffer2 on AudioBuffer1
+            channelBuffer1[sampleIndex] += channelBuffer2[sampleIndex];
+        }
+    }
+}
--- audio/audio_buffer.h
+++ audio/audio_buffer.h
@@ -11,24 +11,76 @@
 template<typename T, size_t CHANNEL_NUM, size_t BUFFER_LEN>
 class AudioBuffer {
 public:
+    /**
+     * Constructor, statically asserts that the length of the
+     * AudioBuffer is even.
+     */
     AudioBuffer() {
         static_assert((BUFFER_LEN % 2) == 0, "The AudioBuffer BUFFER_LEN must be even");
     };
 
+    /**
+     * Returns the channel count.
+     *
+     * @return number of channels of the buffer
+     */
     inline size_t getNumChannels() { return bufferContainer.size(); };
 
+    /**
+     * Returns the length of the buffers (that is the same for
+     * each channel).
+     *
+     * @return buffer length
+     */
     inline size_t getBufferLength() { return bufferContainer[0].size(); };
 
+    /**
+     * Returns a raw pointer to the data array of a certain channel
+     * of the buffer. The access is read only.
+     *
+     * @param channelNumber target channel
+     * @return read pointer to the channel data
+     */
     inline const T *getReadPointer(unsigned int channelNumber) { return bufferContainer[channelNumber].data(); };
 
+    /**
+     * Returns a raw pointer to the data array of a certain channel
+     * of the buffer. The pointer grants both read and write accesses.
+     *
+     * @param channelNumber target channel
+     * @return write pointer to the channel data
+     */
     inline T *getWritePointer(unsigned int channelNumber) { return bufferContainer[channelNumber].data(); };
-    
-    void applyGain(float) {};
+
+    /**
+     * Applies a constant gain to the AudioBuffer.
+     *
+     * @param gain multiplicative factor
+     */
+    void applyGain(float gain); // TODO: test it
+
+    /**
+     * Sums a second AudioBuffer to this AudioBuffer.
+     *
+     * @param buffer AudioBuffer to sum to this instance
+     */
+    void add(AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN> buffer); // TODO: test it
 
 private:
+    /**
+     * Data structure containing the buffer data.
+     */
     std::array <std::array<T, BUFFER_LEN>, CHANNEL_NUM> bufferContainer;
-    AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN>(const AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN>&);
-    AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN> &operator=(const AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN>&);
+
+    /**
+     * Disabling copy constructor.
+     */
+    AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN>(const AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN> &);
+
+    /**
+     * Disabling move operator.
+     */
+    AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN> &operator=(const AudioBuffer<T, CHANNEL_NUM, BUFFER_LEN> &);
 };
 
 // TODO: SIMD implementation
