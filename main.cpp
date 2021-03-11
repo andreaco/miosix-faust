2418d9dbb640e4e45fce01c6419fc141b1fd8cf5
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-11 22:15:21 +0100

    Update main.cpp
    removing tests

--- main.cpp
+++ main.cpp
@@ -33,39 +33,15 @@ public:
 };
 
 int main() {
-    AudioProcessorTest audioProcessorTest;
-//    audioProcessorTest.process();
-
+    
     // initializing the audio driver
     AudioDriver &audioDriver = AudioDriver::getInstance();
     audioDriver.getBuffer();
+    AudioProcessorTest audioProcessorTest;
     audioDriver.setAudioProcessable(audioProcessorTest);
     audioDriver.init(SampleRate::_44100Hz);
 
-    // AudioBuffer copy test
-    AudioBuffer<float, 1, 10> audioBuffer1;
-    AudioBuffer<float, 1, 10> audioBuffer2;
-    float *audioBufferRaw1 = audioBuffer1.getWritePointer(0);
-    for (uint32_t i = 0; i < audioBuffer1.getBufferLength(); i++) {
-        audioBufferRaw1[i] = static_cast<float>(i);
-    }
-    audioBuffer2.copyOnChannel(audioBuffer1, 0);
-    audioBuffer2.add(audioBuffer1);
-    audioBuffer2.applyGain(0.5);
-    audioBuffer2.clear();
 
-    // test LUT
-    AudioMath::LookupTable<16> sineLUT([](float x){return std::sin(x);},
-                                        0.0f, 2.0f * static_cast<float>(M_PI),
-                                        AudioMath::LookupTableEdges::PERIODIC);
-    sineLUT(0.0);
-    sineLUT(M_PI);
-    sineLUT(2 * M_PI);
-    sineLUT(-1);
-    sineLUT(4 * M_PI);
-
-
-    // infinite loop
     for (;;) {
 
     }
