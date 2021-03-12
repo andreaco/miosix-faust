d1c7de4dca0e1eab81c242b7e405542a33335127
Federico Di Marzo <mail.federicodimarzo@gmail.com>
2021-03-12 02:06:43 +0100

    Update midi.cpp/h
    Handling pitchBend, controlChange and masking of channels

--- audio/midi.cpp
+++ audio/midi.cpp
@@ -1,7 +1,6 @@
 
 #include "midi.h"
 #include "midiXparser.h"
-#include "../tests/midi_test_data.h"
 #include "audio_math.h"
 #include <array>
 #include <algorithm>
@@ -17,50 +16,64 @@ float Midi::midi2freq(uint8_t midiNote) {
     return exp2LUT((midiNote - 69.0) / 12.0) * 440.0;
 }
 
-void Midi::testParser() {
-    midiXparser midiParser;
-    uint8_t msg;
-    uint8_t len;
-    uint8_t type;
-    uint8_t note;
-    uint8_t velocity;
-    int tmp = 0;
-    float frequency;
-
-    std::array<uint8_t, 16> readBuffer;
-    uint8_t *readPtr;
-    size_t msgLen = sizeof(TEST_MESSAGE) / sizeof(*TEST_MESSAGE);
-
+Midi::Parser::Parser() : channelMask(0xFFFF) {
     midiParser.setMidiMsgFilter(midiXparser::channelVoiceMsgTypeMsk);
-    for (size_t i = 0; i < msgLen; i++) {
-        msg = TEST_MESSAGE[i];
-        if (midiParser.parse(msg)) {
-            len = midiParser.getMidiMsgLen();
-            type = midiParser.getMidiMsgType();
-            readPtr = midiParser.getMidiMsg();
-            std::copy(readPtr, readPtr + len, std::begin(readBuffer));
+}
+
+Midi::MidiMessage Midi::Parser::parse(uint8_t *&message) {
+    bool messageReadComplete = false;
+    uint8_t currentByte;
+    uint8_t statusByte;
+    midiMessage = MidiMessage::NOT_SUPPORTED; // default fallback
+
+    while (!messageReadComplete) {
+        currentByte = *message;
+        message++;
+        if (midiParser.parse(currentByte)) {
+            messageReadComplete = true; // a message has been parsed
+            messagePtr = midiParser.getMidiMsg();
+            statusByte = messagePtr[0];
+            dataBytes[0] = messagePtr[1];
+            dataBytes[1] = messagePtr[2];
+
+            // checking the channel
+            if (!channelIsEnabled(statusByte & 0xf)) {
+                // channel is masked, breaking out the while
+                midiMessage = MidiMessage::MASKED;
+                break;
+            }
 
             // note message
             if (midiParser.isMidiStatus(midiXparser::noteOnStatus) ||
                 midiParser.isMidiStatus(midiXparser::noteOffStatus)) {
-
-                note = readPtr[1];
-                velocity = readPtr[2];
-                MidiNote midiNote(readPtr);
-                frequency = Midi::midi2freq(midiNote.getNote());
+                note = Note(messagePtr);
 
                 if (midiParser.isMidiStatus(midiXparser::noteOffStatus) || // noteOn with 0 velocity is a noteOff
-                     (midiParser.isMidiStatus(midiXparser::noteOnStatus) && midiNote.getVelocity() == 0)) {
+                    (midiParser.isMidiStatus(midiXparser::noteOnStatus) && note.getVelocity() == 0)) {
                     // note off
-                    tmp--;
+                    midiMessage = MidiMessage::NOTE_OFF;
                 } else {
                     // note on
-                    tmp++;
+                    midiMessage = MidiMessage::NOTE_ON;
                 }
-
+                break;
             }
 
-            // note off
+            // control change
+            if (midiParser.isMidiStatus(midiXparser::controlChangeStatus)) {
+                controllerNumber = dataBytes[0];
+                controllerValue = dataBytes[1];
+                midiMessage = MidiMessage::CONTROL_CHANGE;
+                break;
+            }
+
+            // pitch bend
+            if (midiParser.isMidiStatus(midiXparser::pitchBendStatus)) {
+                pitchBendValue = (dataBytes[0] << 7) | dataBytes[1];
+                midiMessage = MidiMessage::PITCH_BEND;
+                break;
+            }
         }
     }
+    return midiMessage;
 }
