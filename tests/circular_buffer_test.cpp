56635e2b0e5f5764fa618998cfe8410753f6fcee
andreaco <andrea.cppl@gmail.com>
2021-03-29 19:40:05 +0200

    Added Tests for the new CircularBuffer class

--- tests/circular_buffer_test.cpp
+++ tests/circular_buffer_test.cpp
@@ -3,115 +3,118 @@
 
 TEST_CASE("CircularBuffer", "[containers]") {
 
-    SECTION("CircularBuffer shared functionalities") {
-        OverridingCircularBuffer<int, 4> buffer;
+    SECTION("CircularBuffer STL style") {
+        CircularBuffer<int, 4> buffer;
 
         SECTION("putting elements in the buffer") {
-            buffer.put(1);
+            buffer.push_back(1);
             REQUIRE(buffer.front() == 1);
-            buffer.put(2);
+
+            buffer.push_back(2);
             REQUIRE(buffer.front() == 1);
+
 
             SECTION("getting elements") {
-                REQUIRE(buffer.get() == 1);
-                REQUIRE(buffer.get() == 2);
-                REQUIRE(buffer.isEmpty() == true);
-
+                REQUIRE(buffer.front() == 1);
+                buffer.pop_front();
+                REQUIRE(buffer.front() == 2);
+                buffer.pop_front();
+                REQUIRE(buffer.empty() == true);
             }
 
-            SECTION("checking isFull") {
-                OverridingCircularBuffer<int, 2> buffer2;
-                REQUIRE(buffer2.isFull() == false);
-                // filling the buffer
-                buffer2.put(3);
-                buffer2.put(4);
-                REQUIRE(buffer2.isFull() == true);
-            }
 
             SECTION("checking isEmpty") {
-                REQUIRE(buffer.isEmpty() == false);
+                REQUIRE(buffer.empty() == false);
 
                 // clearing the buffer
                 buffer.clear();
-                REQUIRE(buffer.isEmpty() == true);
+                REQUIRE(buffer.empty() == true);
             }
 
             SECTION("checking the buffer length") {
-                REQUIRE(buffer.size() == 4);
+                REQUIRE(buffer.max_size() == 4);
             }
 
             SECTION("checking the current capacity") {
-                REQUIRE(buffer.getCurrentCapacity() == 2);
-                buffer.put(45);
-                REQUIRE(buffer.getCurrentCapacity() == 3);
+                REQUIRE(buffer.size() == 2);
+                buffer.push_back(45);
+                REQUIRE(buffer.size() == 3);
             }
         }
     }
 
-    SECTION("OverridingCircularBuffer test") {
-        OverridingCircularBuffer<float, 4> buffer;
+    SECTION("Overriding CL") {
+        CircularBuffer<float, 4> buffer;
         SECTION("wrapping") {
-            REQUIRE(buffer.isEmpty() == true);
-            buffer.put(1);
-            buffer.put(2);
-            buffer.put(2.5);
-            buffer.get();
-            buffer.get();
-            buffer.put(3.5);
-            REQUIRE(buffer.getCurrentCapacity() == 2);
-            buffer.put(3.6);
-            buffer.put(3.9);
-            buffer.get();
-            buffer.get();
-            REQUIRE(buffer.get() == Approx(3.6));
+            REQUIRE(buffer.empty() == true);
+
+            buffer.push_back(1);
+            buffer.push_back(2);
+            buffer.push_back(2.5);
+
+
+            buffer.pop_front();
+            buffer.pop_front();
+            buffer.push_back(3.5);
+
+            REQUIRE(buffer.size() == 2);
+
+            buffer.push_back(3.6);
+
+            buffer.push_back(3.9);
+
+            buffer.pop_front();
+            buffer.pop_front();
+
+            REQUIRE(buffer.front() == Approx(3.6));
+            buffer.pop_front();
             buffer.clear();
-            REQUIRE(buffer.isEmpty() == true);
-            REQUIRE(buffer.getCurrentCapacity() == 0);
+            REQUIRE(buffer.empty() == true);
+            REQUIRE(buffer.size() == 0);
         }
 
         SECTION("overriding") {
-            buffer.put(1);
-            buffer.put(2);
-            buffer.put(2.5);
-            buffer.put(3.5);
-            buffer.put(4.5);
-            REQUIRE(buffer.get() == Approx(2));
-        }
-    }
-
-    SECTION("Discarding CircularBuffer test") {
-        DiscardingCircularBuffer<double, 4> buffer;
-        SECTION("wrapping") {
-            buffer.put(1);
-            buffer.put(2);
-            buffer.put(2.5);
-            buffer.get();
-            buffer.get();
-            buffer.put(3.5);
-            REQUIRE(buffer.getCurrentCapacity() == 2);
-            buffer.put(3.6);
-            buffer.put(3.9);
-            buffer.get();
-            buffer.get();
-            REQUIRE(buffer.get() == 3.6);
-            REQUIRE(buffer.getCurrentCapacity() == 1);
-            buffer.clear();
-            REQUIRE(buffer.isEmpty() == true);
-            REQUIRE(buffer.getCurrentCapacity() == 0);
-        }
-
-        SECTION("overriding") {
-            buffer.put(1);
-            buffer.put(2);
-            buffer.put(2.5);
-            buffer.put(3.5);
-            buffer.put(4.5);
-            REQUIRE(buffer.get() == 1);
-            REQUIRE(buffer.getCurrentCapacity() == 3);
+            buffer.push_back(1);
+            buffer.push_back(2);
+            buffer.push_back(2.5);
+            buffer.push_back(3.5);
+            buffer.push_back(4.5);
+            REQUIRE(buffer.front() == Approx(2));
         }
 
     }
 
+    SECTION("Discarding CircularBuffer test") {
+        CircularBuffer<double, 4, CircularBufferType::Discard> buffer;
+        SECTION("wrapping") {
+            buffer.push_back(1);
+            buffer.push_back(2);
+            buffer.push_back(2.5);
+            buffer.pop_front();
+            buffer.pop_front();
+            buffer.push_back(3.5);
+            REQUIRE(buffer.size() == 2);
+            buffer.push_back(3.6);
+            buffer.push_back(3.9);
+            buffer.pop_front();
+            buffer.pop_front();
+            REQUIRE(buffer.front() == 3.6);
+            buffer.pop_front();
+            REQUIRE(buffer.size() == 1);
+            buffer.clear();
+            REQUIRE(buffer.empty() == true);
+            REQUIRE(buffer.size() == 0);
+        }
 
-//    SECTION()
+        SECTION("overriding") {
+            buffer.push_back(1);
+            buffer.push_back(2);
+            buffer.push_back(2.5);
+            buffer.push_back(3.5);
+            buffer.push_back(4.5);
+            REQUIRE(buffer.front() == 1);
+            buffer.pop_front();
+            REQUIRE(buffer.size() == 3);
+        }
+    }
 }
