4686b4d4fe822f927c2cfa177049162634fa0a8b
andreaco <andrea.cppl@gmail.com>
2021-03-30 09:49:46 +0200

    Templated MidiParser::parse

--- midi/midi.cpp
+++ midi/midi.cpp
@@ -22,58 +22,6 @@ Midi::MidiParser::MidiParser() : channelMask(0xFFFF) {
     midiParser.setMidiMsgFilter(midiXparser::channelVoiceMsgTypeMsk);
 }
 
-void Midi::MidiParser::parse(std::queue <uint8_t> &rawMidiQueue) {
-    uint8_t currentByte;
-    uint8_t statusByte;
-    std::array<uint8_t, 2> dataBytes;
-    uint8_t *messagePtr;
-    Midi::MidiMessageType midiMessageType = Midi::MidiMessageType::NOT_SUPPORTED; // default fallback
-
-    while (rawMidiQueue.empty() == false) {
-        currentByte = rawMidiQueue.front(); // input read
-        rawMidiQueue.pop();
-        if (midiParser.parse(currentByte)) {
-            messagePtr = midiParser.getMidiMsg();
-            statusByte = messagePtr[0];
-            dataBytes[0] = messagePtr[1];
-            dataBytes[1] = messagePtr[2];
-
-            // checking the channel first
-            if (!channelIsEnabled(statusByte & 0xf)) {
-                // channel is masked, breaking out the while
-                midiMessageType = Midi::MidiMessageType::MASKED;
-
-                // note message -------
-            } else if (midiParser.isMidiStatus(midiXparser::noteOnStatus) ||
-                       midiParser.isMidiStatus(midiXparser::noteOffStatus)) {
-                Note note(dataBytes[0], dataBytes[1]);
-                if (midiParser.isMidiStatus(midiXparser::noteOffStatus) || // noteOn with 0 velocity is a noteOff
-                    (midiParser.isMidiStatus(midiXparser::noteOnStatus) && note.getVelocity() == 0)) {
-                    // note off
-                    midiMessageType = Midi::MidiMessageType::NOTE_OFF;
-                } else {
-                    // note on
-                    midiMessageType = Midi::MidiMessageType::NOTE_ON;
-                }
-
-                // control change -------
-            } else if (midiParser.isMidiStatus(midiXparser::controlChangeStatus)) {
-                midiMessageType = Midi::MidiMessageType::CONTROL_CHANGE;
-
-                // pitch bend -------
-            } else if (midiParser.isMidiStatus(midiXparser::pitchBendStatus)) {
-                midiMessageType = Midi::MidiMessageType::PITCH_BEND;
-
-            } // end of message parsing
-
-            // constructing the new MidiMessage
-            Midi::MidiMessage midiMessage = {dataBytes, midiMessageType};
-            messageQueue.push(midiMessage); // the element is pushed into the queue
-        }
-    }
-
-}
-
 
 Midi::Note Midi::MidiMessage::getNote() {
     return Midi::Note(dataBytes[0], dataBytes[1]);
--- midi/midi.h
+++ midi/midi.h
@@ -5,7 +5,9 @@
 #include "midiXparser.h"
 #include <array>
 #include <list>
-#include <queue>
+#include "../containers/circular_buffer.h"
+
+#define BUFFER_MAX_SIZE 32
 
 /**
  * This namespace defines classes and functions to handle
@@ -80,9 +82,7 @@ namespace Midi {
                 dataBytes(data),
                 midiType(type) {};
 
-
-        MidiMessage() = delete;
-
+        MidiMessage() {}
 //        MidiMessage(const MidiMessage &copy) :
 //                dataBytes(copy.getDataBytes()),
 //                midiType(copy.getType()) {};
@@ -121,11 +121,62 @@ namespace Midi {
          */
         MidiParser();
 
-        void parse(std::queue <uint8_t> &rawMidiQueue);
+        template<class Queue>
+        void parse(Queue &rawMidiQueue) {
+            uint8_t currentByte;
+            uint8_t statusByte;
+            std::array<uint8_t, 2> dataBytes;
+            uint8_t *messagePtr;
+            Midi::MidiMessageType midiMessageType = Midi::MidiMessageType::NOT_SUPPORTED; // default fallback
+
+            while (rawMidiQueue.empty() == false) {
+                currentByte = rawMidiQueue.front(); // input read
+                rawMidiQueue.pop();
+                if (midiParser.parse(currentByte)) {
+                    messagePtr = midiParser.getMidiMsg();
+                    statusByte = messagePtr[0];
+                    dataBytes[0] = messagePtr[1];
+                    dataBytes[1] = messagePtr[2];
+
+                    // checking the channel first
+                    if (!channelIsEnabled(statusByte & 0xf)) {
+                        // channel is masked, breaking out the while
+                        midiMessageType = Midi::MidiMessageType::MASKED;
+
+                        // note message -------
+                    } else if (midiParser.isMidiStatus(midiXparser::noteOnStatus) ||
+                               midiParser.isMidiStatus(midiXparser::noteOffStatus)) {
+                        Note note(dataBytes[0], dataBytes[1]);
+                        if (midiParser.isMidiStatus(midiXparser::noteOffStatus) || // noteOn with 0 velocity is a noteOff
+                            (midiParser.isMidiStatus(midiXparser::noteOnStatus) && note.getVelocity() == 0)) {
+                            // note off
+                            midiMessageType = Midi::MidiMessageType::NOTE_OFF;
+                        } else {
+                            // note on
+                            midiMessageType = Midi::MidiMessageType::NOTE_ON;
+                        }
+
+                        // control change -------
+                    } else if (midiParser.isMidiStatus(midiXparser::controlChangeStatus)) {
+                        midiMessageType = Midi::MidiMessageType::CONTROL_CHANGE;
+
+                        // pitch bend -------
+                    } else if (midiParser.isMidiStatus(midiXparser::pitchBendStatus)) {
+                        midiMessageType = Midi::MidiMessageType::PITCH_BEND;
+
+                    } // end of message parsing
+
+                    // constructing the new MidiMessage
+                    Midi::MidiMessage midiMessage = {dataBytes, midiMessageType};
+                    messageQueue.push(midiMessage); // the element is pushed into the queue
+                }
+            }
+
+        }
 
         MidiMessage popMidiMessage();
 
-        inline std::queue <MidiMessage> getMessageQueue() { return messageQueue; };
+        inline CircularBuffer<MidiMessage, BUFFER_MAX_SIZE> getMessageQueue() { return messageQueue; };
 
         /**
          * Masks a certain channel.
@@ -160,7 +211,7 @@ namespace Midi {
 
     private:
 
-        std::queue <MidiMessage> messageQueue;
+        CircularBuffer<MidiMessage, BUFFER_MAX_SIZE> messageQueue;
 
         /**
          * Bitmask used to enable the channels.
