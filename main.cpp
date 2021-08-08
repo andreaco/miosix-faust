7f2a56c8623659d14e8351f1ce4b63cdb73f5720
andreaco <andrea.cppl@gmail.com>
2021-08-08 18:08:49 +0200

    Added 4 encoders

--- main.cpp
+++ main.cpp
@@ -14,14 +14,17 @@
 #include "drivers/stm32f407vg_discovery/encoder.h"
 #include <util/lcd44780.h>
 
-typedef miosix::Gpio<GPIOB_BASE,12> d4;
-typedef miosix::Gpio<GPIOB_BASE,13> d5;
-typedef miosix::Gpio<GPIOB_BASE,14> d6;
-typedef miosix::Gpio<GPIOB_BASE,15> d7;
-typedef miosix::Gpio<GPIOC_BASE,1> rs;
-typedef miosix::Gpio<GPIOC_BASE,2> e;
+typedef miosix::Gpio<GPIOB_BASE, 12> d4;
+typedef miosix::Gpio<GPIOB_BASE, 13> d5;
+typedef miosix::Gpio<GPIOB_BASE, 14> d6;
+typedef miosix::Gpio<GPIOB_BASE, 15> d7;
+typedef miosix::Gpio<GPIOC_BASE, 1> rs;
+typedef miosix::Gpio<GPIOC_BASE, 2> e;
 
-typedef Encoder<TIM4_BASE, GPIOD_BASE, 12, 13> encoder1;
+typedef Encoder<TIM1_BASE, GPIOE_BASE,  9, 11> encoder1;
+typedef Encoder<TIM3_BASE, GPIOB_BASE,  4,  5> encoder2;
+typedef Encoder<TIM4_BASE, GPIOD_BASE, 12, 13> encoder3;
+typedef Encoder<TIM5_BASE, GPIOA_BASE,  0,  1> encoder4;
 
 int main()
 {
@@ -29,13 +32,19 @@ int main()
     miosix::Lcd44780 display(rs::getPin(),e::getPin(),d4::getPin(),
                              d5::getPin(),d6::getPin(),d7::getPin(),2,16);
     encoder1::init();
+    encoder2::init();
+    encoder3::init();
+    encoder4::init();
 
     for(;;) {
-        miosix::Thread::sleep(100);
-
+        miosix::Thread::sleep(1000);
         display.clear();
         display.go(0, 1);
-        display.printf("Encoder: %05d", encoder1::getValue());
+        display.printf("[%02d][%02d][%02d][%02d]",
+                       encoder1::getValue(),
+                       encoder2::getValue(),
+                       encoder3::getValue(),
+                       encoder4::getValue());
 
     }
 }
