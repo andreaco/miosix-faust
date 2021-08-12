7b18f64d2da9b77adf7913d07488147253db06e7
andreaco <andrea.cppl@gmail.com>
2021-08-12 18:35:32 +0200

    Added falling edge/rising edge functions

--- drivers/stm32f407vg_discovery/button.h
+++ drivers/stm32f407vg_discovery/button.h
@@ -2,12 +2,10 @@
 #define MIOSIX_DRUM_BUTTON_H
 
 template <uint32_t GPIO_BASE, int PIN>
-class Button
-{
+class Button {
 public:
-    static void init()
-    {
-        GPIO_TypeDef* GPIO = (GPIO_TypeDef*) GPIO_BASE;
+    static void init() {
+        GPIO_TypeDef *GPIO = (GPIO_TypeDef *) GPIO_BASE;
 
         // GPIO Setup
         // TODO: GLOBAL ENABLEGPIO
@@ -16,16 +14,42 @@ public:
         GPIO->MODER  &= ~(3 << (PIN * 2)); // setting the GPIO in input mode
         GPIO->PUPDR  &= ~(3 << (PIN * 2)); // reset
         GPIO->OTYPER &= ~(1 << PIN);       // reset
-        GPIO->PUPDR  |=  (2 << (PIN * 2)); // pull-down
+        GPIO->PUPDR  |= (2 << (PIN * 2));  // pull-down
     }
 
-    static bool getState()
+    static bool getState() {
+        GPIO_TypeDef *GPIO = (GPIO_TypeDef *) GPIO_BASE;
+        previousState = (GPIO->IDR & (1 << 1)) >> 1;
+        return previousState;
+    }
+
+    static bool risingEdge()
     {
-        return (GPIOD->IDR & (1 << 1)) >> 1;
+        GPIO_TypeDef *GPIO = (GPIO_TypeDef *) GPIO_BASE;
+        bool currentState = (GPIO->IDR & (1 << 1)) >> 1;
+        bool isRising = false;
+        if (previousState == false && currentState == true)
+            isRising = true;
+        previousState = currentState;
+        return isRising;
     }
 
+    static bool fallingEdge()
+    {
+        GPIO_TypeDef *GPIO = (GPIO_TypeDef *) GPIO_BASE;
+        bool currentState = (GPIO->IDR & (1 << 1)) >> 1;
+
+        bool isFalling = false;
+        if (previousState == true && currentState == false)
+            isFalling = true;
+        previousState = currentState;
+        return isFalling;
+    }
 private:
+    static bool previousState;
 };
 
+template <uint32_t GPIO_BASE, int PIN>
+bool Button<GPIO_BASE, PIN>::previousState = false;
 
 #endif //MIOSIX_DRUM_BUTTON_H
