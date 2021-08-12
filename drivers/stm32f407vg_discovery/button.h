fa809b5807f734e76f2bef668fb2786cbb19b1c9
andreaco <andrea.cppl@gmail.com>
2021-08-12 16:47:51 +0200

    Update button.h

--- drivers/stm32f407vg_discovery/button.h
+++ drivers/stm32f407vg_discovery/button.h
@@ -1,39 +1,6 @@
 #ifndef MIOSIX_DRUM_BUTTON_H
 #define MIOSIX_DRUM_BUTTON_H
 
-void buttonsSetup()
-{
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
-
-    // PIN 1
-    int gpioPin = 1;
-    GPIOD->MODER &= ~(3 << (gpioPin * 2)); // setting the GPIO in input mode
-    GPIOD->PUPDR &= ~(3 << (gpioPin * 2)); // reset
-    GPIOD->OTYPER &= ~(1 << gpioPin); // reset
-    GPIOD->PUPDR |= (2 << (gpioPin * 2)); // pull-down
-
-    // PIN 2
-    gpioPin = 2;
-    GPIOD->MODER &= ~(3 << (gpioPin * 2)); // setting the GPIO in input mode
-    GPIOD->PUPDR &= ~(3 << (gpioPin * 2)); // reset
-    GPIOD->OTYPER &= ~(1 << gpioPin); // reset
-    GPIOD->PUPDR |= (2 << (gpioPin * 2)); // pull-down
-
-    // PIN 3
-    gpioPin = 3;
-    GPIOD->MODER &= ~(3 << (gpioPin * 2)); // setting the GPIO in input mode
-    GPIOD->PUPDR &= ~(3 << (gpioPin * 2)); // reset
-    GPIOD->OTYPER &= ~(1 << gpioPin); // reset
-    GPIOD->PUPDR |= (2 << (gpioPin * 2)); // pull-down
-
-    // PIN 4
-    gpioPin = 4;
-    GPIOD->MODER &= ~(3 << (gpioPin * 2)); // setting the GPIO in input mode
-    GPIOD->PUPDR &= ~(3 << (gpioPin * 2)); // reset
-    GPIOD->OTYPER &= ~(1 << gpioPin); // reset
-    GPIOD->PUPDR |= (2 << (gpioPin * 2)); // pull-down
-}
-
 template <uint32_t GPIO_BASE, int PIN>
 class Button
 {
