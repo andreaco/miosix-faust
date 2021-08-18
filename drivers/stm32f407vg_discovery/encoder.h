e4aa4a9889be0aa3d10d3bdc2ab49561a70ee479
andreaco <andrea.cppl@gmail.com>
2021-08-18 18:20:00 +0200

    Added method to get increment/decrement + relative value between 0 and 1

--- drivers/stm32f407vg_discovery/encoder.h
+++ drivers/stm32f407vg_discovery/encoder.h
@@ -1,6 +1,7 @@
 #ifndef MIOSIX_DRUM_ENCODER_H
 #define MIOSIX_DRUM_ENCODER_H
 #include <miosix.h>
+#include <math.h>
 #include "drivers/stm32f407vg_discovery/utility.h"
 
 /**
@@ -40,6 +41,7 @@ public:
 
             // Value to count up to
             TIM->ARR = 0xFFFF;
+            TIM->CNT = 0x7FFF;
 
             // Setup (RM9000, pg. 616)
             TIM->CCMR1 |= (TIM_CCMR1_CC1S_0 | TIM_CCMR1_CC2S_0);
@@ -58,12 +60,33 @@ public:
      * Get the current value held by the encoder
      * @return Current value
      */
-    static int getValue()
+    static float getValue()
     {
+        // Update value
+        value += getIncrement();
+
+        // Clip value
+        value = std::min(std::max(value, 0.0f), 1.0f);
+        return value;
+    }
+
+    /**
+     * Get how much the encoder has incremented/decremented
+     * Useful for updating different variables from a single encoder
+     * @return Current increment
+     */
+    static float getIncrement()
+    {
+        TIM_TypeDef* TIM = (TIM_TypeDef*) TIM_BASE;
+        uint16_t counterValue;
         {
             miosix::FastInterruptDisableLock dLock;
-            return ((TIM_TypeDef *) TIM_BASE)->CNT >> 2;
+            // Read Counter
+            counterValue = TIM->CNT;
+            // Reset Counter
+            TIM->CNT = 0x7FFF;
         }
+        return (counterValue - 0x7FFF) / 1000.0f;
     }
 
 private:
@@ -81,9 +104,17 @@ private:
      * Assignment operator disabled
      */
     Encoder &operator=(const Encoder &);
+
+    /**
+     * Current Stored Value
+     */
+    static float value;
 };
-/*
-template <uint32_t GPIO_BASE, int PIN>
-bool Button<GPIO_BASE, PIN>::previousState = false;
-*/
+
+/**
+ * Static initialization of associated float value
+ */
+template <uint32_t TIM_BASE, uint32_t GPIO_BASE, int PIN1, int PIN2>
+float Encoder<TIM_BASE, GPIO_BASE, PIN1, PIN2>::value = 0.0f;
+
 #endif //MIOSIX_DRUM_ENCODER_H
