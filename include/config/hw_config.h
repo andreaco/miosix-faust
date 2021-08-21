d425e6cded7156dae80a6908b4a6803ac4150089
andreaco <andrea.cppl@gmail.com>
2021-08-21 17:11:46 +0200

    Update hw_config.h

--- include/config/hw_config.h
+++ include/config/hw_config.h
@@ -28,16 +28,16 @@
  * (2): USART2: tx=PA2  rx=PA3  cts=PA0  rts=PA1
  * (3): USART3: tx=PB10 rx=PB11 cts=PB13 rts=PB14
  */
-#define MIDI_SERIAL_ID 1
+#define MIDI_SERIAL_ID (1)
 
 /**
  * MIDI notes circular buffer size
  */
-#define MIDI_NOTE_MESSAGE_QUEUE_SIZE 32
+#define MIDI_NOTE_MESSAGE_QUEUE_SIZE (32)
 
 /**
  * MIDI CC circular buffer size
  */
-#define CC_MESSAGE_QUEUE_SIZE 32
+#define CC_MESSAGE_QUEUE_SIZE (32)
 
 #endif //MIOSIX_AUDIO_HW_CONFIG_H
